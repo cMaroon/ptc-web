b963f00f5e1beed97c7cc9651143cd34664f96d7
Emmanuel See Te <bsit.emmanuel@gmail.com>
2018-06-08 16:41:28 +0800

    changelog-20180608

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -48,6 +48,7 @@ class DashboardController extends Controller
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
+     * 
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -59,6 +60,7 @@ class DashboardController extends Controller
      * Display the specified resource.
      *
      * @param int $id
+     * 
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -70,6 +72,7 @@ class DashboardController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param int $id
+     * 
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -82,6 +85,7 @@ class DashboardController extends Controller
      *
      * @param \Illuminate\Http\Request $request
      * @param int $id
+     * 
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -93,6 +97,7 @@ class DashboardController extends Controller
      * Remove the specified resource from storage.
      *
      * @param int $id
+     * 
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
