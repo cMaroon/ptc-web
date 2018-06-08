8506032a03914f6a5b5a3a440a48a1133903cf15
Emmanuel See Te <bsit.emmanuel@gmail.com>
2018-06-08 16:40:36 +0800

    changelog-20180608

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -47,7 +47,7 @@ class DashboardController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
